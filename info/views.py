05f395de117c066d85fff8c3fc996c613bfb0c78
wo <1251868103@qq.com>
2019-07-01 09:52:20 +0800

    ignore

--- info/views.py
+++ info/views.py
@@ -5,7 +5,11 @@ from django.core.serializers.json import DjangoJSONEncoder
 from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 
-from django.contrib.auth import authenticate, login as django_login
+from django.contrib.auth import (
+    authenticate,
+    login as django_login,
+    logout as django_logout,
+)
 
 from django.conf import settings
 
@@ -22,62 +26,64 @@ import json
 
 
 def index_page(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def manage_page(request):
-    return render(request, 'back.html')
+    return render(request, "back.html")
 
 
 def info_list_page(request):
-    return render(request, 'list.html')
+    return render(request, "list.html")
 
 
 def get_csrftoken(request):
     token = get_token(request)
-    response = JsonResponse({'detail': '登录成功'})
-    response.set_cookie('csrftoken', token)
+    response = JsonResponse({"detail": "登录成功"})
+    response.set_cookie("csrftoken", token)
     return response
 
 
 @decorator_token()
 def list_info(request):
-    page = request.GET.get('pageNumber', 1)
+    page = request.GET.get("pageNumber", 1)
 
-    info_objects = Info.objects.order_by('-create_time', 'clinical_name')
+    info_objects = Info.objects.order_by("-create_time", "clinical_name")
 
     paginator_object = Paginator(info_objects, settings.PER_PAGE_NUMS)
 
-    total_numbers = paginator_object.count   # 总数
-    total_pages = paginator_object.num_pages   # 总页数
+    total_numbers = paginator_object.count  # 总数
+    total_pages = paginator_object.num_pages  # 总页数
     c = paginator_object.page_range  # 页数范围
 
     d = paginator_object.get_page(page)
 
-    serializer = serialize('json', d.object_list, cls=DjangoJSONEncoder)
+    serializer = serialize("json", d.object_list, cls=DjangoJSONEncoder)
 
     info_list = json.loads(serializer)
 
-    response_info = [info['fields'] for info in info_list]
+    response_info = [info["fields"] for info in info_list]
 
-    return JsonResponse({
-        'data': response_info,
-        'total_numbers': total_numbers,
-        'username': request.user.username,
-    })
+    return JsonResponse(
+        {
+            "data": response_info,
+            "total_numbers": total_numbers,
+            "username": request.user.username,
+        }
+    )
 
 
 def create_info(request):
     data = query_to_dict(request.POST)
 
     if not data:
-        return JsonResponse({'detail': '提交内容为空'}, status=400)
+        return JsonResponse({"detail": "提交内容为空"}, status=400)
 
     if not any(list(data.values())):
-        return JsonResponse({'detail': '提交内容为空'}, status=400)
+        return JsonResponse({"detail": "提交内容为空"}, status=400)
 
     info = Info(**data)
-    response_data = ''
+    response_data = ""
 
     try:
         info.full_clean()
@@ -85,56 +91,60 @@ def create_info(request):
 
         for k in e.message_dict.keys():
             if k in error_messages.keys():
-                response_data += '%s<br />' % error_messages.get(k)
+                response_data += "%s<br />" % error_messages.get(k)
 
-        return JsonResponse({'detail': response_data}, status=400)
+        return JsonResponse({"detail": response_data}, status=400)
 
     info.save()
 
-    return JsonResponse({'detail': '提交信息成功'})
+    return JsonResponse({"detail": "提交信息成功"})
 
 
 def login(request):
     data = query_to_dict(request.POST)
     now_time = datetime.datetime.now()
-    expired_time = datetime.datetime.now() + datetime.timedelta(days=settings.EXPIRED_TIME)
+    expired_time = datetime.datetime.now() + datetime.timedelta(
+        days=settings.EXPIRED_TIME
+    )
     # expired_time = datetime.datetime.now() + datetime.timedelta(seconds=60)
 
     user = authenticate(**data)
 
     if user is None:
-        return JsonResponse({'detail': '账号或密码错误'}, status=400)
+        return JsonResponse({"detail": "账号或密码错误"}, status=400)
 
     django_login(request, user)
 
     token = jwt.encode(
-        {
-            'data': data,
-            'exp': expired_time,
-            'iat': now_time
-        },
+        {"data": data, "exp": expired_time, "iat": now_time},
         settings.SECRET_KEY,
-        algorithm='HS256')
+        algorithm="HS256",
+    )
 
     token_objs = Token.objects.filter(u_id=user.id)
 
     if token_objs:
         try:
-            token_objs.update(token=token.decode('utf-8'))
+            token_objs.update(token=token.decode("utf-8"))
         except Exception:
-            return JsonResponse({'detail': '账号或密码错误'}, status=400)
+            return JsonResponse({"detail": "账号或密码错误"}, status=400)
     else:
         try:
-            tk_object = Token(token=token.decode('utf-8'), u_id=user.id)
+            tk_object = Token(token=token.decode("utf-8"), u_id=user.id)
             tk_object.save()
         except Exception:
-            return JsonResponse({'detail': '账号或密码错误'}, status=400)
+            return JsonResponse({"detail": "账号或密码错误"}, status=400)
 
-    response = JsonResponse({'detail': '登录成功'})
-    response.set_cookie('token', token.decode('utf-8'), expires=expired_time)
+    response = JsonResponse({"detail": "登录成功"})
+    response.set_cookie("token", token.decode("utf-8"), expires=expired_time)
     return response
 
 
-@decorator_token('logout')
+@decorator_token("logout")
 def logout(request):
-    return JsonResponse({'detail': '退出登录'})
+    try:
+        django_logout(request)
+    except Exception:
+        return JsonResponse({"detail": "退出错误"}, status=400)
+
+    return JsonResponse({"detail": "退出登录"})
--- static/js/info-list.js
+++ static/js/info-list.js
@@ -1,73 +1,89 @@
-$(function(){
-    var jwt = getCookie('token');
-    $("#pagination").pagination({
-        dataSource: '/info/',
-        locator: "data",
-        showGoInput: true,
-        showGoButton: true,
-        totalNumberLocator: function(resp) {
-            $(".user-info-name").text(resp.username);
-            return resp.total_numbers;
-        },
-        formatAjaxError: function(jqXHR, textStatus, errorThrown) {
-            alert(jqXHR.responseText);
-            window.location.href="/manage/";
-        },
-        ajax: {
-            type: "GET",
-            headers: {
-                'JWT-Token': jwt,
-            }
-        },
-        callback: function(data, pagination) {
-            var inhtml = '';
-            var innertext = [
-                '<tr>'+
-                    '<th>诊所名称</th>'+
-                    '<th>医生姓名</th>'+
-                    '<th>医生电话</th>'+
-                    '<th>患者姓名</th>'+
-                    '<th>年龄/性别</th>'+
-                    '<th>患者电话</th>'+
-                    '<th>患者描述</th>'+
-                '</tr>',
-            ];
-            for(var i=0;i<data.length;i++){
-                inhtml += '<a>'+data[i].clinical_name+'</a><br/>';
+$(function() {
+  var jwt = getCookie("token");
+  $("#pagination").pagination({
+    dataSource: "/info/",
+    locator: "data",
+    showGoInput: true,
+    showGoButton: true,
+    totalNumberLocator: function(resp) {
+      $(".user-info-name").text(resp.username);
+      return resp.total_numbers;
+    },
+    formatAjaxError: function(jqXHR, textStatus, errorThrown) {
+      alert(jqXHR.responseText);
+      window.location.href = "/manage/";
+    },
+    ajax: {
+      type: "GET",
+      headers: {
+        "JWT-Token": jwt
+      }
+    },
+    callback: function(data, pagination) {
+      var inhtml = "";
+      var innertext = [
+        "<tr>" +
+          "<th>诊所名称</th>" +
+          "<th>医生姓名</th>" +
+          "<th>医生电话</th>" +
+          "<th>患者姓名</th>" +
+          "<th>年龄/性别</th>" +
+          "<th>患者电话</th>" +
+          "<th>患者描述</th>" +
+          "</tr>"
+      ];
+      for (var i = 0; i < data.length; i++) {
+        inhtml += "<a>" + data[i].clinical_name + "</a><br/>";
 
-                innertext.push('<tr>'+
-                                    '<td>'+data[i].clinical_name+'</td>'+
-                                    '<td>'+data[i].doc_name+'</td>'+
-                                    '<td>'+data[i].doc_phone+'</td>'+
-                                    '<td>'+data[i].patient_name+'</td>'+
-                                    '<td>'+data[i].sex_age+'</td>'+
-                                    '<td>'+data[i].patient_phone+'</td>'+
-                                    '<td>'+data[i].patient_detail+'</td>'+
-                                '</tr>');
-            }
+        innertext.push(
+          "<tr>" +
+            "<td>" +
+            data[i].clinical_name +
+            "</td>" +
+            "<td>" +
+            data[i].doc_name +
+            "</td>" +
+            "<td>" +
+            data[i].doc_phone +
+            "</td>" +
+            "<td>" +
+            data[i].patient_name +
+            "</td>" +
+            "<td>" +
+            data[i].sex_age +
+            "</td>" +
+            "<td>" +
+            data[i].patient_phone +
+            "</td>" +
+            "<td>" +
+            data[i].patient_detail +
+            "</td>" +
+            "</tr>"
+        );
+      }
 
-            $('.info-table').html(innertext.join(''));
-        }
-    });
-})
-
-function logout(){
-    var jwt = getCookie('token');
-    var is_out = confirm("是否退出当前账号");
-
-    if(is_out){
-        $.ajax({
-            url: "/logout/",
-            type: "get",
-            headers: {
-                'JWT-Token': jwt,
-            },
-            success: function(resp, txt, xhr){
-                window.location.href = "/manage/";
-            },
-            error: function(jqXHR, txt, status){
-                $(".error-info").html(jqXHR.responseJSON.detail);
-            },
-        });
+      $(".info-table").html(innertext.join(""));
     }
+  });
+});
+
+function logout() {
+  var jwt = getCookie("token");
+  var is_out = confirm("是否退出当前账号");
+
+  if (is_out) {
+    $.ajax({
+      url: "/logout/",
+      type: "get",
+      headers: {
+        "JWT-Token": jwt
+      },
+      success: function(resp, txt, xhr) {
+        window.location.href = "/manage/";
+      },
+      error: function(jqXHR, txt, status) {
+        alert(jqXHR.responseJSON.detail);
+      }
+    });
+  }
 }
