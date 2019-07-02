5112ee116ceac54e3da369493c93d30f4079f094
wo <1251868103@qq.com>
2019-07-02 10:23:24 +0800

    .

---
+++ info/constants.py
@@ -0,0 +1,9 @@
+xlsx_header = {
+    "clinical_name": "诊所名称",
+    "doc_name": "医生姓名",
+    "doc_phone": "医生电话",
+    "patient_name": "患者姓名",
+    "sex_age": "年龄/性别",
+    "patient_phone": "患者电话",
+    "patient_detail": "患者描述",
+}
